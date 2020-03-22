0e481c386c34b078e60b65e0a8bd3751d1d47e4a
DvDRaga <dvdragazzi@gmail.com>
2020-03-22 20:53:20 +0100

    Changes

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function toggleCredits() {
+    if (document.getElementById("credits").style.display === "none") {
+        document.getElementById("credits").style.display = "flex"
+    } else {
+        document.getElementById("credits").style.display = "none"
+    }
+}
