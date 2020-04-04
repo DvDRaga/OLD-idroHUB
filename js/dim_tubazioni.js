8f1b42832e034656912ae659cdeaef84dc6ffeee
David <dvdragazzi@gmail.com>
2020-04-04 21:44:40 +0200

    Script tubazioni

--- js/dim_tubazioni.js
+++ js/dim_tubazioni.js
@@ -1,3 +1,15 @@
-var cell = document.getElementsByTagName("td")[0].innerHTML
-//console.log(cell)
-const calc = () => cell = "CIAO"
+var calc = () => {
+    var port = document.getElementById("portata").value
+    var temp = document.getElementById("temperatura").value
+    var ind = data.acqua.temp.indexOf(Number(temp))
+    
+    for (i = 0; i < 9; i++) {
+        var perdCell = document.getElementsByTagName("td")[i * 2]
+        var velCell = document.getElementsByTagName("td")[i * 2 + 1]
+
+        var perd = 14.7 * Math.pow(data.acqua.visc[ind], 0.25) * data.acqua.dens[ind] * Math.pow(port, 1.75) / Math.pow(data.diamTub[i], 4.75)
+        perdCell.innerHTML = Math.ceil(perd) + " mm/m"
+        var vel = port / (Math.PI * Math.pow(data.diamTub[i], 2) * 3.6 / 4)
+        velCell.innerHTML = vel.toFixed(2) + " m/s"
+    }
+}
